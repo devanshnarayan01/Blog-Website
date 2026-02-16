dc12567c679a4a376faf5799ed011977939b5794
Devansh Narayn Singh <yashrajputt23@gmail.com>
2026-02-16 19:34:17 +0530

    Blog Website

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
